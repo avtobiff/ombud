27bd72c2e69808584c4957cd23d7364e007a16df
Per Andersson <avtobiff@gmail.com>
2016-02-14 15:11:53 +0100

    Add Python script to send multiple commands on one socket

---
+++ util/multicmd.py
@@ -0,0 +1,23 @@
+import socket
+import sys
+
+
+def send_cmds(sock, commands):
+    sock.send(commands)
+
+    try:
+        while True:
+            data = sock.recv(8192)
+            if not data: break
+            sys.stdout.write(data)
+    except socket.timeout:
+        print "timeout"
+
+
+sock = socket.socket()
+sock.settimeout(1)
+sock.connect(("localhost", 8090))
+
+cmds = ["localhost:22", "127.0.0.1:22", "localhost:9999", "127.0.0.1:9999"]
+send_cmds(sock, "\r\n".join(cmds))
+send_cmds(sock, "\n".join(cmds))
