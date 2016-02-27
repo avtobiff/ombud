2db5d7da08612d5d0d98f5bc3fd69bf38b3aa06e
Per Andersson <avtobiff@gmail.com>
2016-02-27 05:32:46 +0100

    Close multi cmd script client socket

--- util/multicmd.py
+++ util/multicmd.py
@@ -21,3 +21,5 @@ sock.connect(("localhost", 8090))
 cmds = ["localhost:22", "127.0.0.1:22", "localhost:9999", "127.0.0.1:9999"]
 send_cmds(sock, "\r\n".join(cmds))
 send_cmds(sock, "\n".join(cmds))
+
+sock.close()
